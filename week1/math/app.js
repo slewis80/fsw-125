7b6df956e59ef268ef851191f97ea877d81f7a35
Sabrina Lewis <sabrina.lewis@bryanuniversity.edu>
2020-06-12 13:08:07 -0500

    week3 server

---
+++ week1/math/app.js
@@ -0,0 +1,13 @@
+const math = require("./math");
+
+console.log(math.add(11, 22));
+// 33
+
+console.log(math.subtract(50, 10));
+// 40
+
+console.log(math.multiply(5, 5));
+// 25
+
+console.log(math.divide(10, 2));
+// 5
